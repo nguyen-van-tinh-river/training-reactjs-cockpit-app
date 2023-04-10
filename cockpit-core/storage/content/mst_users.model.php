3ecec222d30a918582d8373acbc486feed287a3f
Nguyễn Văn Tính <nguyen.tinh.rcvn2012@gmail.com>
2023-04-10 13:38:32 +0700

    remove ignore git

---
+++ cockpit-core/storage/content/mst_users.model.php
@@ -0,0 +1,17 @@
+<?php
+ return [
+  'name' => 'mst_users',
+  'label' => 'Users',
+  'info' => '',
+  'type' => 'collection',
+  'fields' => [
+  ],
+  'preview' => [
+  ],
+  'group' => '',
+  'meta' => NULL,
+  '_created' => 1681108593,
+  '_modified' => 1681108593,
+  'color' => NULL,
+  'revisions' => false,
+];
